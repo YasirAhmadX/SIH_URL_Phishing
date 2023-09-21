e911454fed7e165eef4e70d18bfd3f5f1eaccb71
Yasir Ahmad <yasir.ahmad0810@gmail.com>
2023-09-21 17:37:13 +0530

    set k = 3

--- scripts/kNNmodel.py
+++ scripts/kNNmodel.py
@@ -53,7 +53,7 @@ train = train.drop('time_domain_activation', axis=1)
 data = train.drop('phishing', axis=1)
 label = train['phishing']
 X_train, X_val, y_train, y_val = train_test_split(data,label,test_size=0.2, random_state=42)
-knn = KNeighborsClassifier(n_neighbors=2)
+knn = KNeighborsClassifier(n_neighbors=3)
 knn.fit(X_train, y_train)
 # pic.dump(knn,open('knn.pkl','wb'))
 prediction = knn.predict(X_val)
