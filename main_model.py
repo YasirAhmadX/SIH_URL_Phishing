e6d790d3cf6453ad78ff2361831f3e0b7b1b9d22
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-09-21 13:48:17 +0530

    Add files via upload

---
+++ main_model.py
@@ -0,0 +1,64 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, roc_curve, confusion_matrix, classification_report, roc_auc_score
+
+def display_test_scores(test, pred):
+    str_out = ""
+    str_out += ("TEST SCORES")
+    str_out += ("\n")
+
+    #print accuracy
+    accuracy = accuracy_score(test, pred)
+    str_out += ("ACCURACY: {:.4f}\n".format(accuracy))
+    str_out += ("\n")
+
+    #print AUC score
+    auc = roc_auc_score(test, pred)
+    str_out += ("AUC: {:.4f}\n".format(auc))
+    str_out += ("\n")
+
+    #print confusion matrix
+    str_out += ("CONFUSION MATRIX:\n")
+    conf_mat = confusion_matrix(test, pred)
+    str_out += ("{}".format(conf_mat))
+    str_out += ("\n")
+    str_out += ("\n")
+
+    #print FP, FN
+    str_out += ("FALSE POSITIVES:\n")
+    fp = conf_mat[1][0]
+    pos_labels = conf_mat[1][0]+conf_mat[1][1]
+    str_out += ("{} out of {} positive labels ({:.4f}%)\n".format(fp, pos_labels,fp/pos_labels))
+    str_out += ("\n")
+
+    str_out += ("FALSE NEGATIVES:\n")
+    fn = conf_mat[0][1]
+    neg_labels = conf_mat[0][1]+conf_mat[0][0]
+    str_out += ("{} out of {} negative labels ({:.4f}%)\n".format(fn, neg_labels, fn/neg_labels))
+    str_out += ("\n")
+
+    #print classification report
+    str_out += ("PRECISION, RECALL, F1 scores:\n")
+    str_out += ("{}".format(classification_report(test, pred)))
+    
+    false_indexes = np.where(test != pred)
+    return str_out, false_indexes
+
+
+train=pd.read_csv("newdataset.csv")
+train = train.drop('time_domain_expiration', axis=1)
+train = train.drop('time_domain_activation', axis=1)
+data = train.drop('phishing', axis=1)
+label = train['phishing']
+X_train, X_val, y_train, y_val = train_test_split(data,label,test_size=0.2, random_state=42)
+knn = KNeighborsClassifier(n_neighbors=2)
+knn.fit(X_train, y_train)
+# pic.dump(knn,open('knn.pkl','wb'))
+prediction = knn.predict(X_val)
+results,false = display_test_scores(y_val, prediction)
+print(results)
+
+
+
